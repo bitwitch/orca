f8af6004ed71f23365d87a76952dd71f37aca679
Reuben Dunnington <reuben.dunnington@gmail.com>
2024-01-27 21:52:14 -0800

    replace stb sprintf with musl version

--- src/orca-libc/src/stdio/sprintf.c
+++ src/orca-libc/src/stdio/sprintf.c
@@ -1,3 +1,12 @@
-#define STB_SPRINTF_DECORATE(name) name
-#define STB_SPRINTF_IMPLEMENTATION
-#include "stb_sprintf.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+int sprintf(char *restrict s, const char *restrict fmt, ...)
+{
+	int ret;
+	va_list ap;
+	va_start(ap, fmt);
+	ret = vsprintf(s, fmt, ap);
+	va_end(ap);
+	return ret;
+}
