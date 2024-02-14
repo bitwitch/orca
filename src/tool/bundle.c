1e811021c6821e774b09351777e0485625da6157
Shaw <shawwian@gmail.com>
2024-02-14 02:44:01 -0500

    c-tooling: bundle command, update paths to runtime

--- src/tool/bundle.c
+++ src/tool/bundle.c
@@ -137,8 +137,8 @@ int winBundle(
     //-----------------------------------------------------------
     //NOTE: copy orca runtime executable and libraries
     //-----------------------------------------------------------
-    oc_str8 orcaExe = oc_path_append(a, orcaDir, OC_STR8("build/bin/orca_runtime.exe"));
-    oc_str8 orcaLib = oc_path_append(a, orcaDir, OC_STR8("build/bin/orca.dll"));
+    oc_str8 orcaExe = oc_path_append(a, orcaDir, OC_STR8("bin/orca_runtime.exe"));
+    oc_str8 orcaLib = oc_path_append(a, orcaDir, OC_STR8("bin/orca.dll"));
     oc_str8 glesLib = oc_path_append(a, orcaDir, OC_STR8("src/ext/angle/bin/libGLESv2.dll"));
     oc_str8 eglLib = oc_path_append(a, orcaDir, OC_STR8("src/ext/angle/bin/libEGL.dll"));
 
@@ -226,8 +226,8 @@ int macBundle(
     //-----------------------------------------------------------
     //NOTE: copy orca runtime executable and libraries
     //-----------------------------------------------------------
-    oc_str8 orcaExe = oc_path_append(a, orcaDir, OC_STR8("build/bin/orca_runtime"));
-    oc_str8 orcaLib = oc_path_append(a, orcaDir, OC_STR8("build/bin/liborca.dylib"));
+    oc_str8 orcaExe = oc_path_append(a, orcaDir, OC_STR8("bin/orca_runtime"));
+    oc_str8 orcaLib = oc_path_append(a, orcaDir, OC_STR8("bin/liborca.dylib"));
     oc_str8 glesLib = oc_path_append(a, orcaDir, OC_STR8("src/ext/angle/bin/libGLESv2.dylib"));
     oc_str8 eglLib = oc_path_append(a, orcaDir, OC_STR8("src/ext/angle/bin/libEGL.dylib"));
 
